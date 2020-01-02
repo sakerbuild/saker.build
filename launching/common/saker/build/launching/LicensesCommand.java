ca990ef6a0db3a192ff11e2cbe97cf5e875efee0
Bence Sipka <10866741+Sipkab@users.noreply.github.com>
2020-01-02 11:37:35 +0100

    Specify license (#4)
    * Create LICENSE
    
    * Add license notices to the sources, embed LICENSE in release JAR
    
    Update licenses command to display the license as well

--- launching/common/saker/build/launching/LicensesCommand.java
+++ launching/common/saker/build/launching/LicensesCommand.java
@@ -1,7 +1,23 @@
+/*
+ * Copyright (C) 2020 Bence Sipka
+ *
+ * This program is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package saker.build.launching;
 
 import java.io.InputStream;
 
+import saker.build.meta.Versions;
 import saker.build.thirdparty.saker.util.io.StreamUtils;
 
 /**
@@ -11,6 +27,16 @@ import saker.build.thirdparty.saker.util.io.StreamUtils;
  */
 public class LicensesCommand {
 	public void call() throws Exception {
+		System.out.println("Saker.build system version " + Versions.VERSION_STRING_FULL);
+		System.out.println("Copyright (C) 2020 Bence Sipka");
+		System.out.println();
+		try (InputStream in = Main.class.getClassLoader().getResourceAsStream("META-INF/LICENSE")) {
+			StreamUtils.copyStream(in, System.out);
+		}
+		System.out.println();
+		System.out.println("----------");
+		System.out.println("Embedded software licenses");
+		System.out.println();
 		try (InputStream in = Main.class.getClassLoader().getResourceAsStream("META-INF/BUNDLED-LICENSES")) {
 			StreamUtils.copyStream(in, System.out);
 		}
