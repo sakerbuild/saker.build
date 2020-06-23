993358b3a72149f56ec8ed976211d0ddeb18ffd8
sipkab <10866741+Sipkab@users.noreply.github.com>
2020-06-23 17:21:20 +0200

    Bump version 0.8.14

--- core/common/saker/build/meta/Versions.java
+++ core/common/saker/build/meta/Versions.java
@@ -56,7 +56,7 @@ public class Versions {
 	 * @see <a href="https://semver.org/">https://semver.org/</a>
 	 */
 	@PublicApi(unconstantize = DefaultableBoolean.TRUE)
-	public static final int VERSION_PATCH = 13;
+	public static final int VERSION_PATCH = 14;
 
 	/**
 	 * The full version string in the format of
