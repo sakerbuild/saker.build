f52f25cfb7b78652d420734c6b1de442e189bf1d
sipkab <10866741+Sipkab@users.noreply.github.com>
2020-07-17 17:26:07 +0200

    Fix javadoc references

--- thirdparty/asm/src/saker/build/thirdparty/org/objectweb/asm/package-info.java
+++ thirdparty/asm/src/saker/build/thirdparty/org/objectweb/asm/package-info.java
@@ -30,27 +30,26 @@
 /**
  * Provides a small and fast bytecode manipulation framework.
  * <p>
- * The <a href="http://asm.ow2.org/">ASM</a> framework is organized around the {@link org.objectweb.asm.ClassVisitor
- * ClassVisitor}, {@link org.objectweb.asm.FieldVisitor FieldVisitor}, {@link org.objectweb.asm.MethodVisitor
- * MethodVisitor} and {@link org.objectweb.asm.AnnotationVisitor AnnotationVisitor} abstract classes, which allow one to
- * visit the fields, methods and annotations of a class, including the bytecode instructions of each method.
+ * The <a href="http://asm.ow2.org/">ASM</a> framework is organized around the {@link ClassVisitor ClassVisitor},
+ * {@link FieldVisitor FieldVisitor}, {@link MethodVisitor MethodVisitor} and {@link AnnotationVisitor
+ * AnnotationVisitor} abstract classes, which allow one to visit the fields, methods and annotations of a class,
+ * including the bytecode instructions of each method.
  * <p>
- * In addition to these main abstract classes, ASM provides a {@link org.objectweb.asm.ClassReader ClassReader} class,
- * that can parse an existing class and make a given visitor visit it. ASM also provides a
- * {@link org.objectweb.asm.ClassWriter ClassWriter} class, which is a visitor that generates Java class files.
+ * In addition to these main abstract classes, ASM provides a {@link ClassReader ClassReader} class, that can parse an
+ * existing class and make a given visitor visit it. ASM also provides a {@link ClassWriter ClassWriter} class, which is
+ * a visitor that generates Java class files.
  * <p>
- * In order to generate a class from scratch, only the {@link org.objectweb.asm.ClassWriter ClassWriter} class is
- * necessary. Indeed, in order to generate a class, one must just call its visit<em>Xxx</em> methods with the
- * appropriate arguments to generate the desired fields and methods.
+ * In order to generate a class from scratch, only the {@link ClassWriter ClassWriter} class is necessary. Indeed, in
+ * order to generate a class, one must just call its visit<em>Xxx</em> methods with the appropriate arguments to
+ * generate the desired fields and methods.
  * <p>
- * In order to modify existing classes, one must use a {@link org.objectweb.asm.ClassReader ClassReader} class to
- * analyze the original class, a class modifier, and a {@link org.objectweb.asm.ClassWriter ClassWriter} to construct
- * the modified class. The class modifier is just a {@link org.objectweb.asm.ClassVisitor ClassVisitor} that delegates
- * most of the work to another {@link org.objectweb.asm.ClassVisitor ClassVisitor}, but that sometimes changes some
- * parameter values, or call additional methods, in order to implement the desired modification process. In order to
- * make it easier to implement such class modifiers, the {@link org.objectweb.asm.ClassVisitor ClassVisitor} and
- * {@link org.objectweb.asm.MethodVisitor MethodVisitor} classes delegate by default all the method calls they receive
- * to an optional visitor.
+ * In order to modify existing classes, one must use a {@link ClassReader ClassReader} class to analyze the original
+ * class, a class modifier, and a {@link ClassWriter ClassWriter} to construct the modified class. The class modifier is
+ * just a {@link ClassVisitor ClassVisitor} that delegates most of the work to another {@link ClassVisitor
+ * ClassVisitor}, but that sometimes changes some parameter values, or call additional methods, in order to implement
+ * the desired modification process. In order to make it easier to implement such class modifiers, the
+ * {@link ClassVisitor ClassVisitor} and {@link MethodVisitor MethodVisitor} classes delegate by default all the method
+ * calls they receive to an optional visitor.
  * 
  * @since ASM 1.3
  */
