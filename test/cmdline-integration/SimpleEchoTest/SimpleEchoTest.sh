2ad8ee211f682a018345bd4f7432f27d9ed95ab5
sipkab <10866741+Sipkab@users.noreply.github.com>
2021-01-03 19:09:04 +0100

    Print test build_output.txt files as grep fails to find line on CI

--- test/cmdline-integration/SimpleEchoTest/SimpleEchoTest.sh
+++ test/cmdline-integration/SimpleEchoTest/SimpleEchoTest.sh
@@ -2,6 +2,8 @@ set -e
 
 java -jar $SAKER_BUILD_JAR_PATH | tee build_output.txt
 
+xxd build_output.txt
+
 if grep -q "hello world" build_output.txt; then
 	echo "Printed message not found."
 	exit 1
