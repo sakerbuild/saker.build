e13b494ebe717423c15ac556c5bd61bbf28d8105
sipkab <10866741+Sipkab@users.noreply.github.com>
2021-08-21 21:29:52 +0200

    Closables emptiness check improvement

--- test/runner/src/testing/saker/SakerJavaTestingInvoker.java
+++ test/runner/src/testing/saker/SakerJavaTestingInvoker.java
@@ -198,10 +198,10 @@ public class SakerJavaTestingInvoker extends BasicInstrumentationJavaTestInvoker
 			while (true) {
 				AutoCloseable c;
 				synchronized (SakerJavaTestingInvoker.class) {
-					if (closeables.isEmpty()) {
+					c = closeables.pollLast();
+					if (c == null) {
 						break;
 					}
-					c = closeables.pollLast();
 				}
 				try {
 					c.close();
